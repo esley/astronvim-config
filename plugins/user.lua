9556f87358127b98d68fc3f5765277179b52f9ce
Esley Madeira <esley.madeira@fonnet.com.br>
2024-03-15 20:39:21 -0300

    adicao de tema novo

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,11 +2,12 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "catppuccin/nvim", name = "catppuccin", priority = 1000, config = function() require("catppuccin").setup({
+    flavour = "frappe", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    }),
+  },
 }
