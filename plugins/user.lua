568ed8a707c0e56865ae25c26967f1616cc219dc
Esley Madeira <esley.madeira@fonnet.com.br>
2024-03-15 22:26:00 -0300

    correcao ao utilizar tema

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,13 +1,14 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
+  -- Você também pode adicionar novos plugins aqui:
+  -- Adicione plugins, a sintaxe preguiçosa
   -- "andweeb/presence.nvim",
   {
     "catppuccin/nvim", name = "catppuccin", priority = 1000, config = function() require("catppuccin").setup({
-    flavour = "frappe", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
+      flavour = "frappe", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
         light = "latte",
         dark = "mocha",
-    }),
-  },
+      }
+    }) end, -- Fechamento da função config()
+  }
 }
